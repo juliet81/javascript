80f8660a8376b0f66b66307bce22dc29e2722e55
Julieta Parra <parrajuliet@hotmail.com>
2022-10-22 04:41:00 -0300

    capetas de clases

---
+++ clases/clase1-variables.js
@@ -0,0 +1,117 @@
+/*
+
+                        VARIABLES
+
+//esto me sirve para mostrar un dato en la consola del navegador
+console.log("bienvenido a Js");
+
+//alert("hola mundo")
+
+console.log("juli");
+console.log("paloma");
+
+var nombre;
+
+nombre = "eleonora";
+
+console.log(nombre);
+
+var numero = 10;
+
+numero = 15;
+
+numero = "20";
+
+var nuero2 = 10.5; 
+
+console.log(numero);
+
+var apellido1 = "Benitez";
+var apellido2 = "a";
+var apellido3 = "Roque";
+
+apellido3 = "marta";
+
+console.log(apellido3);
+
+const PI = 3.1415;
+
+//PI = 3.1517
+
+const IVA = 21;
+const DNI = 45494876;
+const TEMP = 100;
+
+//
+
+var apellido3 = "julia";
+
+console.log(apellido3);
+
+let apellido4 = "ortega";
+
+apellido4 = "torres";
+
+//let apellido4 = "torres";
+
+let anio;
+let telMovil;
+
+//const SIEMPRE MAYUSCULA
+
+// char letra = "a"
+// string cadena "nombre compuesto"
+
+//suma resta multiplicacion division modulo
+let numero1 = 10;
+let numero2 = 20;
+
+console.log(numero1+numero2);
+console.log(numero1-numero2);
+console.log(numero1/numero2);
+console.log(numero1*numero2);
+
+let nombreA = "Leandro";
+let apellidoB = "Ruiz";
+
+//conecatenar
+console.log(nombreA + apellidoB);
+
+//concatenar (si trabajamos con + con tipo dato caracteres, sino los suma)
+console.log(nombreA + " " + apellidoB);
+console.log("El usuario es: " + nombreA + " " + apellidoB);
+console.log(nombreA + numero1);
+console.log((numero1 + numero2)+nombreA);
+*/
+
+//PROMPT
+/* 
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese su apellido");
+
+console.log("el usuario ingreso: " + nombre + "apellido" + apellido);
+
+alert("el usuario ingreso:" + nombre);
+
+//los prompt por defecto String o cadena de caracteres
+let numero01 = Number (prompt("ingrese un numero"));
+
+console.log(numero01);
+
+//parseo
+//numero01 = Number(numero01);
+
+//console.log(numero01+numero01)
+
+let numero02 = Number (prompt("ingrese otro numero"));
+
+console.log(numero01+numero02);
+*/
+
+//var edadJulio = Number (prompt("ingrese su edad")); 
+
+//para que redondee en caso de comas o puntos ej 30.5 redondea en 30
+var edadJulio = parseInt (prompt("ingrese su edad")); 
+let promedio = 0;
+console.log(edadJulio);
+
